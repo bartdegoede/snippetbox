edeb705e7061cfbf130e28afa9107885c808241a
Bart de Goede <bart@scribd.com>
2020-02-16 15:19:56 -0800

    Info and error logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,41 +4,35 @@ import (
 	"log"
 	"net/http"
 	"flag"
+	"os"
 )
 
 func main() {
-	// Define a command-line flag with the name "addr", a default value of "localhost:4000"
-	// and some short help text explaining what the flag controls. The value of the
-	// flag will be stored in the addr variable at runtime.
 	addr := flag.String("addr", "localhost:4000", "HTTP network address")
-
-	// Importantly, we use the flag.Parse() function to parse the command-line flag.
-	// This reads the command-line flag value and assigns it to the addr
-	// variable. We'll need to assign this *before* we use the addr variable,
-	// otherwise it will always contain the default value of "localhost:4000". If any errors are
-	// encountered during the parsing the application will be terminated.
 	flag.Parse()
+
+	// Use the log.New() to create a logger for writing information messages. This takes
+	// three parameters: the destination to write the logs to (os.Stdout), a string
+	// prefix for message (INFO followed by a tab) and flags to indicate what
+	// additional information to include (local date and time). Note that the flags
+	// are joined using the bitwise OR operator |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a log for writing error messages in the same way, but use stderr as
+	// the destination and use the log.Lshortfile flag to include the relevant
+	// file name and line number.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the
-	// projcet root.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// The value returned from the flag.String() function is a pointer to the
-	// flag value, not the value itself. So we need to dereference the pointer
-	// (ie prefix it with *) before using it. Note that we're using the
-	// log.Printf() function to interpolate the address with the log message
-	log.Printf("Starting server on %s", *addr)
+	// Write messages using the two new loggers, instead of the standard logger
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
