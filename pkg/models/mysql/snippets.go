f3b1bd907249400840576b0ce0f69d7b33e38bce
Bart de Goede <bart@scribd.com>
2020-02-17 22:57:44 -0800

    Insert snippet SQL

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -6,22 +6,43 @@ import (
 	"bartdegoe.de/snippetbox/pkg/models"
 )
 
-// define a SnippetModel type which wraps a sql.DB connection pool
 type SnippetModel struct {
 	DB *sql.DB
 }
 
-// this wil insert a new snippet into the database
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	// Write the SQL statement we want to execute. It's split over two lines
+	// for readability, which is why it's surrounded by backquotes instead
+	// of normal double quotes.
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// Use the Exec() method on the embedded connection pool to execute the
+	// statement. The first parameter is the SQL statement, followed by the
+	// title, content and expiry values for the placeholder parameters. This
+	// method returns a sql.Result object, which contains some basic information
+	// about what happened when the statement was executed.
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method on the result object to get the ID of the
+	// newly inserted record in the snippets table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has the type int64, so we convert it to an int type
+	// before returning
+	return int(id), nil
 }
 
-// this will return a specific snippet based on its id
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 	return nil, nil
 }
 
-// this will return the 10 most recently created snippets
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
 	return nil, nil
 }
