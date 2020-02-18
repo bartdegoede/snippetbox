e785464472ffe650934a4f6e6828a11d694868b6
Bart de Goede <bart@scribd.com>
2020-02-17 18:51:51 -0800

    Isolate routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,20 +28,10 @@ func main() {
 		infoLog: infoLog,
 	}
 
-	// Swap the route declarations to use the application struct's methods as
-	// the handler functions
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.routes(), // call new app.routes() instead
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
