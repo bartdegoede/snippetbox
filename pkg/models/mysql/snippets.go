c8319c5eb6176847d35b83d81c612986043ed5e2
Bart de Goede <bart@scribd.com>
2020-02-19 22:48:52 -0800

    Refactor snippet fetching code

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -32,33 +32,17 @@ func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-	// Use the QueryRow() method on the connection pool to execute our
-	// SQL statement, passing in the untrusted id variable as the value for
-	// placeholder parameter. This returns a pointer to a sql.Row object which
-	// holds the result from the database.
-	row := m.DB.QueryRow(stmt, id)
-
-	// Initialize a pointer to a new zeroed Snippet struct.
 	s := &models.Snippet{}
 
-	// Use row.Scan() to copy the values from each field in sql.Row to the
-	// corresponding field in the Snippet struct. Notice that the arguments
-	// to row.Scan are *pointers* to the place you want to copy the data into,
-	// and the number of arguments must be exactly the same as the number of
-	// columns returned by the statement.
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	err := m.DB.QueryRow(stmt, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
 	if err != nil {
-		// If the query returns no rows, then row.Scan() will return a
-		// sql.ErrorNoRows error. We use the errors.Is() function to check for
-		// that error specifically, and return our own models.ErrNoRecord error
-		// instead.
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, models.ErrNoRecord
 		}
 		return nil, err
 	}
 
-	// If everything is okay, return the Snippet object
 	return s, nil
 }
 
